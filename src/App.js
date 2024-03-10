e58324738e8159384c4ee28b15e640337a378ac9
Shivam <kpsshivam.gondxi@gmail.com>
2024-03-10 09:46:05 +0530

    final commit

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import Meal from './Components/Meal'
+import './Components/style.css'
+import { Routes, Route } from "react-router-dom"
+import RecipeInfo from './Components/RecipeInfo';
+function App() {
+  return (
+    <>
+      <Routes>
+        <Route path="/" element={<Meal />} />
+        <Route path="/:MealId" element = {<RecipeInfo/>}/>
+      </Routes>
+
+    </>
+  );
+}
+
+export default App;
